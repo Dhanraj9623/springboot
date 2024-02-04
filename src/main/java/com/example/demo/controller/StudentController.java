88152d548176c093d2abb7ad044c5e8622cb23bb
Dhanraj9623 <dhanrajkadam5503@gmail.com>
2024-02-04 20:39:21 +0530

    Added path variable

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -16,5 +16,21 @@ public class StudentController {
         Student student = new Student(1,"Dhanraj","Kadam");
         return student;
     }
+    @GetMapping("students") // http://localhost:8080/students
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(2,"Arya","Kadam"));
+        students.add(new Student(3,"asd","qwe"));
+        return students;
+    }
+
+    //Spring boot rest api with path variable
+    @GetMapping("students/{id}")  // {id} - is called URI template variable ....gets replaced by function input
+    // http://localhost:8080/students/2
+    public Student studentPathVariable(@PathVariable int id){
+        return new Student (id,"Arya","Kadam");
+    }
+
+
 
 }
